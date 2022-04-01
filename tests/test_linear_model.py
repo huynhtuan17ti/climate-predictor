3ddbe6660c140ad5b3b9f9cc51e10c8308476fdb
dotrann1412 <dotrann.1412@gmail.com>
2022-04-01 11:32:55 +0700

    update test function

--- tests/test_linear_model.py
+++ tests/test_linear_model.py
@@ -4,23 +4,18 @@ import numpy as np
 
 def test_linear_regression():
     # TODO: @Do, also update a unit-test for linear model
-    df = pd.DataFrame([(1, 2, 3, 4, 5, 6), 
-                    (2.8, 4.2, 6, 8, 10, 12), 
-                    (3.12, 6, 9.1, 12, 15, 18),
-                    (4.5, 8.5, 12, 16, 20, 24.12),
-                    (5.5, 10, 15, 20.12, 25, 30),
-                    (6.5, 12, 18, 24, 30.15, 36),
-                    (6.5, 12.3, 18, 24, 30.15, 36),
-                    (6.6, 12.2, 18, 24, 30.15, 36),
-                    (6.7, 11.9, 18, 24, 30.15, 36)
-                ], 
-                columns = ('TMIN', 'TMAX', 'TAVG', 'x', 'y', 'z'))
-
+    dfi = np.array([[1, 2, 3], 
+                    [2.8, 4.2, 6], 
+                    [3.12, 6, 9.1],
+                    [4.5, 8.5, 12],
+                    [5.5, 10, 15],
+                    [6.5, 12, 18],
+                    [6.5, 12.3, 18],
+                    [6.6, 12.2, 18],
+                    [6.7, 11.9, 18]
+                ])
+    dfo = np.array([[4],[8],[12],[16],[20],[24], [24.1], [24.2], [24.01]])
     model = LinearModel()
-    dfi = df.copy()
-    for i in ['TMIN', 'TMAX', 'TAVG']:
-        dfi.pop(i)
-
-    weight = model.calc_weight(dfi, df['TMIN'])
-    print(weight)
-    print(model.predict(np.array([[24, 30.15, 36]], dtype = np.int64)))
+    weight = model.calc_weight(dfi, dfo)
+    print('weight: ', weight)
+    print('predict for ', np.array([[6, 11, 19]]), '\nresult: ' ,model.predict(np.array([[6, 11, 19]], dtype = np.int64)))
