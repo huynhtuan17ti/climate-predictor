e8022f9e5c1b551e30e9f7f90e9c40304644c8d2
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-04-01 10:25:08 +0700

    todo jobs

--- src_1/predictor.py
+++ src_1/predictor.py
@@ -4,12 +4,12 @@ import pickle
 
 class LinearModel():
     '''
-        TODO: @Do
         Ax = b => x = b*A^-1
-        b: (TMIN, TMAX, TACG)
+        b: only one feature
     '''
 
     def __init__(self):
+        # TODO: each model [TMIN, TMAX, TAVG] should have a specific weight
         self.weight = np.array
         # 0: for TMIN
         # 1: for TMAX
@@ -18,6 +18,11 @@ class LinearModel():
 
     # dataframe just include features that in linear with TMIN, TMAX, TAVG
     def calc_weight(self, df: pd.DataFrame):
+        # TODO: @Do, the format
+        '''
+            input: np.ndarray (x)
+            output: np.ndarray (b)
+        '''
         (y_tmin, y_tmax, y_tavg) = (df['TMIN'].to_numpy(), df['TMAX'].to_numpy(), df['TAVG'].to_numpy())
         for s in ['TMIN', 'TMAX', 'TAVG']:
             df.pop(s)
@@ -39,22 +44,3 @@ class LinearModel():
     def load(self, pickle_file_name):
         with open(pickle_file_name, 'rb') as handle:
             self.weight = pickle.load(handle)
-
-'''
-df = pd.DataFrame([(1, 2, 3, 4, 5, 6), 
-                    (2.8, 4.2, 6, 8, 10, 12), 
-                    (3.12, 6, 9.1, 12, 15, 18),
-                    (4.5, 8.5, 12, 16, 20, 24.12),
-                    (5.5, 10, 15, 20.12, 25, 30),
-                    (6.5, 12, 18, 24, 30.15, 36),
-                    (6.5, 12.3, 18, 24, 30.15, 36),
-                    (6.6, 12.2, 18, 24, 30.15, 36),
-                    (6.7, 11.9, 18, 24, 30.15, 36)
-                ], 
-                columns = ('TMIN', 'TMAX', 'TAVG', 'x', 'y', 'z'))
-model = LinearModel()
-weight = model.calc_weight(df)
-for line in weight:
-    print(line)
-print(model.predict(np.array([[24, 30.15, 36]], dtype = np.int64)))
-'''
---
+++ tests/test_linear_model.py
@@ -0,0 +1,22 @@
+from src_1.predictor import LinearModel
+import pandas as pd
+import numpy as np
+
+def test_linear_regression():
+    # TODO: @Do, also update a unit-test for linear model
+    df = pd.DataFrame([(1, 2, 3, 4, 5, 6), 
+                    (2.8, 4.2, 6, 8, 10, 12), 
+                    (3.12, 6, 9.1, 12, 15, 18),
+                    (4.5, 8.5, 12, 16, 20, 24.12),
+                    (5.5, 10, 15, 20.12, 25, 30),
+                    (6.5, 12, 18, 24, 30.15, 36),
+                    (6.5, 12.3, 18, 24, 30.15, 36),
+                    (6.6, 12.2, 18, 24, 30.15, 36),
+                    (6.7, 11.9, 18, 24, 30.15, 36)
+                ], 
+                columns = ('TMIN', 'TMAX', 'TAVG', 'x', 'y', 'z'))
+    model = LinearModel()
+    weight = model.calc_weight(df)
+    for coeff in weight:
+        print(coeff)
+    print(model.predict(np.array([[24, 30.15, 36]], dtype = np.int64)))
