41525b21713073d86206d9c921a5190cdeac647b
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-04-06 16:36:12 +0700

    update unit test

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -1,8 +1,8 @@
-from src_1.metric import MSE
+from src_1.metric import MAPE
 import numpy as np
 
-def test_MSE():
+def test_MAPE():
     pred = np.array([1, 1, 0, 0, 5])
     target = np.array([1, 1, 2, -1, 3])
 
-    assert MSE(pred, target) == 1
+    assert MAPE(pred, target) == 1
