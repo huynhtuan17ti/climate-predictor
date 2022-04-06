aef7acc7bbc95cc34d6d6f3e623dd136d26c59bb
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-04-06 16:40:45 +0700

    update unit test

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -1,8 +1,8 @@
-from src_1.metric import MAPE
+from src_1.metric import MAE
 import numpy as np
 
-def test_MAPE():
+def test_MAE():
     pred = np.array([1, 1, 0, 0, 5])
     target = np.array([1, 1, 2, -1, 3])
 
-    assert MAPE(pred, target) == 1
+    assert MAE(pred, target) == 1
