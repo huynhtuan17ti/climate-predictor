f6ad8e4eaf765b6096d6d641b3e5d2b2331fd95c
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-04-07 17:55:54 +0700

    update

--- tests/test_file.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def test_data():
-    assert os.path.exists('data/train.csv')
-    assert os.path.exists('data/test.csv')
--- tests/test_metric.py
+++
@@ -1,8 +0,0 @@
-from src_1.metric import MAE
-import numpy as np
-
-def test_MAE():
-    pred = np.array([1, 1, 0, 0, 5])
-    target = np.array([1, 1, 2, -1, 3])
-
-    assert MAE(pred, target) == 1
